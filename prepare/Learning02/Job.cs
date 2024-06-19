204b65796b019f02f18515ff28d1d6f41b8347f3
Gabriel <66342077+gghayes98@users.noreply.github.com>
2024-06-19 11:42:26 -0600

    Prepare 02

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning02
+{
+    public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        }
+    }
+}
