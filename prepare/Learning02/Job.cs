feefe8bb7e658aac28d6de80443bd07e02ec590d
Killef <gghayes98@gmail.com>
2024-06-22 22:20:00 -0600

    I think this doesn't break things.

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,3 +20,26 @@ namespace Learning02
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning02
+{
+    public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        }
+    }
+}
+>>>>>>> 204b65796b019f02f18515ff28d1d6f41b8347f3
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,3 +23,29 @@ namespace Learning02
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning02
+{
+    public class Resume
+    {
+        public string _name;
+        public List<Job> _jobs = new List<Job>();
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs:");
+            foreach (Job job in _jobs)
+            {
+                job.Display();
+            }
+        }
+    }
+}
+>>>>>>> 204b65796b019f02f18515ff28d1d6f41b8347f3
